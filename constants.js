b264a0e4b4b530f36a5a7130aef00e095c49e606
Ryan Lane <ryanml1999@gmail.com>
2019-08-27 15:07:07 -0700

    v20190827b

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const TITLE = "Cyclone Simulator";
-const VERSION_NUMBER = "20190827a";
+const VERSION_NUMBER = "20190827b";
 
 const SAVE_FORMAT = 3;  // Format #3 in use starting in v20190823a
 const EARLIEST_COMPATIBLE_FORMAT = 0;
