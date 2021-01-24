da377e7df4e56bf91c36aecd2055f0193e985d15
Ryan Lane <ryanml1999@gmail.com>
2021-01-23 21:19:14 -0800

    Added class for longitude-latitude coordinates

--- misc.js
+++ misc.js
@@ -105,6 +105,37 @@ function hashCode(str){
     return hash;
 }
 
+class Coordinate{
+    constructor(longitude, latitude){
+        this.set(longitude, latitude);
+    }
+
+    set(long, lat){
+        this.longitude = ((long + 180) % 360 + 360) % 360 - 180;
+        this.latitude = constrain(lat, -90, 90);
+    }
+
+    add(long, lat){
+        if(long instanceof Coordinate){
+            lat = long.latitude;
+            long = long.longitude;
+        }
+        this.set(this.longitude + long, this.latitude + lat);
+    }
+
+    // for simplicity, returns the equirectangular map projection distance in "Pythagorean degrees" rather than true spherical distance, which is good enough for tropical cyclones far from the poles
+    dist(long, lat){
+        if(long instanceof Coordinate){
+            lat = long.latitude;
+            long = long.longitude;
+        }
+        let long_dist = abs(this.longitude - long);
+        long_dist = min(long_dist, 360 - long_dist);
+        let lat_dist = abs(this.latitude - lat);
+        return Math.hypot(long_dist, lat_dist);
+    }
+}
+
 function loadImg(path){     // wrap p5.loadImage in a promise
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
