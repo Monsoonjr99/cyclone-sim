01f93ceba571398366f62f7dd756b4a27d34f0a2
Ryan Lane <ryanml1999@gmail.com>
2021-08-22 23:19:43 -0700

    Stub simulation module

---
+++ src/simulation.ts
@@ -0,0 +1,27 @@
+import { deepClone } from "./util";
+
+interface StormData{
+    phi: number;
+    lambda: number;
+    pressure: number;
+    windSpeed: number;
+}
+
+interface ActiveStorm extends StormData{
+    id: number;
+}
+
+interface SimulationState{
+    liveTick: number;
+    activeStorms: ActiveStorm[];
+}
+
+function state(toCopy?: SimulationState): SimulationState{
+    if(toCopy)
+        return deepClone<SimulationState>({}, toCopy);
+    else
+        return {
+            liveTick: 0,
+            activeStorms: []
+        };
+}
