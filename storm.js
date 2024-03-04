54e85550aec58a61917c5554843c2d5271ae8734
Ryan Lane <ryanml1999@gmail.com>
2024-03-03 23:39:57 -0800

    track forecast cone optimization (specific to p5.js 1.9.1)

--- storm.js
+++ storm.js
@@ -344,17 +344,9 @@ class Storm{
                 coneSegment(96);
                 coneSegment(120);
                 
-                forecastTracks.loadPixels();
-                for(let i = 0; i < forecastTracks.pixels.length; i += 4){
-                    if(forecastTracks.pixels[i + 3] > 0)
-                        forecastTracks.pixels[i + 3] = 128;
-                }
-                forecastTracks.updatePixels();
-
-                    // Use this instead after upgrading to p5.js 1.9.1
-                // forecastTracks.erase(128, 0);
-                // forecastTracks.rect(0, 0, WIDTH, HEIGHT);
-                // forecastTracks.noErase();
+                forecastTracks.erase(128, 0);
+                forecastTracks.rect(0, 0, WIDTH, HEIGHT);
+                forecastTracks.noErase();
             }
         }
     }
